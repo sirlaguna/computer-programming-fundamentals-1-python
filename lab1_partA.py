98ffbec2b9a561df47e75a9d3fd8d1220c45f087
Suzanne Ashcraft <laguna3939@yahoo.com>
2016-05-04 18:08:31 -0500

    first commit

---
+++ lab1_partA.py
@@ -0,0 +1,6 @@
+Python 2.7.11 |Anaconda 2.4.0 (64-bit)| (default, Dec  6 2015, 18:08:32) 
+[GCC 4.4.7 20120313 (Red Hat 4.4.7-1)] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print('Hello World!')
+Hello World!
+>>> 
